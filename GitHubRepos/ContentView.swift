9aaeb1e1bbf66add737dcef5b54c5c9b12fa445f
Ahmed Eldyahi <ahmed.eldyahi@mdi.com.eg>
2025-07-28 22:36:56 +0300

    Initial Commit

---
+++ GitHubRepos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubRepos
+//
+//  Created by Ahmed Eldyahi on 28/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubRepos/GitHubReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposApp.swift
+//  GitHubRepos
+//
+//  Created by Ahmed Eldyahi on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
